0e636672d40d31d3259df843abd6ae51164f5b3b
Mary Shchur <noreply.tbrm@gmail.com>
2023-03-24 17:17:02 +0200

    Create entity and add database connection

---
+++ src/main/java/org/example/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package org.example.config;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "datasource")
+@Setter
+public class DatabaseConfig {
+    private String url;
+    private String driver;
+    private String username;
+    private String password;
+
+    @Primary
+    @Bean
+    public DataSource customDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,40 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    public String imageUrl;
+
+    @Column(unique = true, length = 50,nullable = false)
+    private String email;
+
+    @Column
+    private String location;
+
+    @Column(unique = true, length = 20,nullable = false)
+    private String phone;
+
+    private boolean status;
+
+}
